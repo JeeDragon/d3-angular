3d502a3bc746faa7b7c4e4817915e0e44b7b53e1
Braden Klunker <bra_nker@hotmail.com>
2020-09-16 08:10:05 -0600

    updated failures graph

--- src/app/d3-failures-graph/d3-failures-graph.component.ts
+++ src/app/d3-failures-graph/d3-failures-graph.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
+import { json, thresholdFreedmanDiaconis } from 'd3';
 
 @Component({
   selector: 'app-d3-failures-graph',
@@ -23,9 +24,10 @@ export class D3FailuresGraphComponent implements OnInit {
       }
     ]
   };
-  private height = 600;
+  private height = 1200;
   private width = 1200;
   private simulation;
+  private svg;
   constructor() { }
 
   ngOnInit(): void {
@@ -37,11 +39,40 @@ export class D3FailuresGraphComponent implements OnInit {
     const links = root.links();
     const nodes = root.descendants();
 
+
+
+
+    this.svg = d3.select('figure#graph1')
+      .append('svg')
+      .attr('viewBox', `0, 0, ${ this.width }, ${ this.height }`)
+      .style('font', '22px sans-serif')
+      .style('user-select', 'none');
+
+    const link = this.svg.append('g')
+      .attr('stroke', '#999')
+      .attr('stroke-opacity', 0.6)
+      .selectAll('line')
+      .data(links)
+      .join('line')
+      .classed('link', true);
+
+    const node = this.svg.append('g')
+      .attr('fill', '#fff')
+      .attr('stroke', '#000')
+      .attr('stroke-width', 1.5)
+      .selectAll('circle')
+      .data(nodes)
+      .join('circle')
+      .attr('fill', d => d.children ? null : '#000')
+      .attr('stroke', d => d.children ? null : '#fff')
+      .attr('r', 10)
+      .classed('node', true)
+      .classed('fixed', (d: any) => d.fx !== undefined);
+
     this.simulation = d3
       .forceSimulation(nodes as any)
       .force('charge', d3.forceManyBody())
       .force('link', d3.forceLink(links as any))
-      // .force('center', d3.forceCenter(this.width / 2, this.height / 2))
       .on('tick', () => {
         link
           .attr('x1', (d: any) => d.source.x)
@@ -54,66 +85,64 @@ export class D3FailuresGraphComponent implements OnInit {
           .attr('cy', (d: any) => d.y);
       });
 
+    // .style('transform', `translate(10px, 50px)`)
+    // .call(drag as any);
+
+    // .append('text')
+    //   .attr('dy', '.35em')
+    //   .attr('text-anchor', 'start')
+    //   .style('fill', 'steelblue')
+    //   .text('Sev3');
+
+    const node2 = this.svg.selectAll('.node')
+      .data(nodes)
+      .enter()
+      .append('g')
+      .attr('class', 'node');
+    // .call(drag);
+
+    // node.append('text')
+    //   .attr('dy', '0.31em')
+    //   // .attr('x', d => d.children ? -6 : 6)
+    //   .attr('text-anchor', 'start')
+    //   .style('fill', 'steelblue')
+    //   .text((d: any) => {
+    //     console.log(d);
+    //     return d.data.name;
+    //   });
+
+    const nodeLabels = node2.append('svg:text')
+      .classed('node-label', true)
+      .attr('dy', 24)
+      .attr('text-anchor', 'middle')
+      .text(d => d.data.name);
+
+    node2.attr('transform', (d: any) => `translate(${ d.x }, ${ d.y })`);
+
+    // node.append('title')
+    //   .text(d => d.data.name);
+
+
     const drag = d3
       .drag()
       .on('start', this.dragstarted)
       .on('drag', this.dragged)
       .on('end', this.dragended);
-
-    const svg = d3.select('figure#graph1')
-      .append('svg')
-      .attr('viewBox', `${ -this.width / 2 }, ${ -this.height / 2 }, ${ this.width }, ${ this.height }`)
-      .style('font', '22px sans-serif')
-      .style('user-select', 'none');
-
-    const link = svg.append('g')
-      .attr('stroke', '#999')
-      .attr('stroke-opacity', 0.6)
-      .selectAll('line')
-      .data(links)
-      .join('line')
-      .classed('link', true);
-
-    const node = svg.append('g')
-      .attr('fill', '#fff')
-      .attr('stroke', '#000')
-      .attr('stroke-width', 1.5)
-      .selectAll('circle')
-      .data(nodes)
-      .join('circle')
-      .attr('fill', d => d.children ? null : '#000')
-      .attr('stroke', d => d.children ? null : '#fff')
-      .attr('r', 10)
-      .classed('node', true)
-      .classed('fixed', (d: any) => d.fx !== undefined)
-      // .style('transform', `translate(10px, 50px)`)
-      .call(drag as any);
-
-    node.append('text')
-      .attr('dy', '0.31em')
-      .attr('x', d => d.children ? -6 : 6)
-      .attr('text-anchor', d => d.children ? 'end' : 'start')
-      .text(d => d.data.name)
-      .clone(true).lower()
-      .attr('stroke', 'white');
-
-    node.append('title')
-      .text(d => d.data.name);
-
-
-
-    node.call(drag as any)
+    node
+      .call(drag as any)
       .on('click', this.click);
 
-    // d3.invalidation.then(() => simulation.stop());
+    // d3.invalidation.then(() => this.simulation.stop());
 
-    return svg.node();
+    return this.svg.node();
   }
 
-  click = (event, d) => {
+  click(event, d) {
+    console.log(event);
+    console.log(d);
     delete d.fx;
     delete d.fy;
-    d3.select(this as any).classed('fixed', false);
+    // this.svg.classed('fixed', false);
     this.simulation.alpha(1).restart();
   }
 
